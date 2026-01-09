394aa2f3fab5ebcedd0d9c88d435a9c91ac0d78a
Joseph Long <joseph.long@sitecore.com>
2026-01-09 08:21:12 +0000

    Changed providers back.

--- headapps/apps/nextjsstarter/src/Providers.tsx
+++ headapps/apps/nextjsstarter/src/Providers.tsx
@@ -1,15 +1,22 @@
 "use client";
 import React from "react";
-import { Page, SitecoreProvider } from "@sitecore-content-sdk/nextjs";
+import {
+  ComponentPropsCollection,
+  ComponentPropsContext,
+  Page,
+  SitecoreProvider,
+} from "@sitecore-content-sdk/nextjs";
 import scConfig from "sitecore.config";
 import components from ".sitecore/component-map.client";
 
 export default function Providers({
   children,
   page,
+  componentProps = {},
 }: {
   children: React.ReactNode;
   page: Page;
+  componentProps?: ComponentPropsCollection;
 }) {
   return (
     <SitecoreProvider
@@ -18,7 +25,9 @@ export default function Providers({
       page={page}
       loadImportMap={() => import(".sitecore/import-map.client")}
     >
-      {children}
+      <ComponentPropsContext value={componentProps}>
+        {children}
+      </ComponentPropsContext>
     </SitecoreProvider>
   );
 }
