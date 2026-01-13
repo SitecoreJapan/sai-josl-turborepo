50e88dc07284f2c3bfcb7ab219afa60ebfb0db7f
Joseph Long <joseph.long@sitecore.com>
2026-01-13 08:51:47 +0000

    Removed config.

--- headapps/apps/nextjsstarter/sitecore.config.ts
+++ headapps/apps/nextjsstarter/sitecore.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-export default defineConfig({
-  defaultSite: process.env.NEXT_PUBLIC_DEFAULT_SITE_NAME || 'sxastarter',
-  defaultLanguage: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE || 'en',
-});
+export default defineConfig({});
